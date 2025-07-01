63c10cbd52ca160c16f9b855911db4b6d2683165
tytia77 <tytia77@gmail.com>
2025-07-01 17:35:52 +1000

    added

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,130 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Test } from './utils';
-import { log } from 'console';
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Test } from "./utils";
+
+import { Window } from "./components/index";
+
+import { throttle, debounce } from "./utils";
 
 function App() {
+  // const prom = new Promise(function(res, rej){
+  //   setTimeout(function(){
+  //     res({})
+  //   }, 5000)
+  // })
 
-  const prom = new Promise(function(res, rej){
-    setTimeout(function(){
-      res({})
-    }, 5000)
-  })
-
-  let test = new Test('sup my g')
-    .reverse()
-    .greet()
-
+  // let test = new Test('sup my g')
+  //   .reverse()
+  //   .greet()
 
   // own implementation of Array.bind
-  function bind(this: any, ...args: any){
-    let obj: Function = this
-    let params = args.slice(1)
+  // function bind(this: any, ...args: any){
+  //   let obj: Function = this
+  //   let params = args.slice(1)
 
-    return function(...args2: any){
-      obj.apply(args[0], [...params, ...args2])
+  //   return function(...args2: any){
+  //     obj.apply(args[0], [...params, ...args2])
+  //   }
+  // }
+
+  // const closure = (a: any) => {
+  //   let aa = a;
+
+  //   return (b?: any) => {
+  //     if (b) {
+  //       aa = aa + b
+  //     }
+
+  //     console.log({aa})
+  //   }
+  // }
+
+  // let c = closure(3);
+  // let a = closure(100);
+
+  // a()
+  // c()
+
+  // a(2)
+  // a(-2)
+  // c()
+
+  // queueMicrotask
+
+
+  const [menuX, setmenuX] = React.useState(0)
+  const [menuY, setmenuY] = React.useState(0)
+
+  const [test, settest]: any = React.useState([])
+
+
+  const [initialMenuPosX, setinitialMenuPosX] = React.useState(0)
+  const [initialMenuPosY, setinitialMenuPosY] = React.useState(0)
+
+  function handlemouseDown(e: React.MouseEvent<HTMLElement>) {
+
+    setinitialMenuPosX(e.clientX - menuX)
+    setinitialMenuPosY(e.clientY - menuY)
+    console.log({ e });
+  }
+
+  function handleMouseMove(this: any, e: React.MouseEvent<HTMLElement>) {
+    if (e.buttons) {
+      setmenuX(e.clientX - initialMenuPosX);
+      setmenuY(e.clientY - initialMenuPosY);
     }
   }
 
-  console.log('tester');
-  
-
-  const closure = (a: any) => {
-    let aa = a;
-
-    return (b?: any) => {
-      if (b) {
-        aa = aa + b
-      }
-
-      console.log({aa})
-    }
+  function handleMouseUp(e: React.MouseEvent<HTMLElement>) {
+    setinitialMenuPosX(0)
+    setinitialMenuPosY(0)
   }
 
-  let c = closure(3);
-  let a = closure(100);
+  function handleKeyUp(e: React.MouseEvent<HTMLElement>) {
+    console.log('fetching data');
+  }
 
-  a()
-  c()
+  function getRand(max: number, min?: number) {
+    return Math.floor(Math.random() * (max - (min || 0) + 1) + (min || 0))
+  }
 
-  a(2)
-  a(-2)
-  c()
 
+  function handleButton(e: React.MouseEvent<HTMLElement>) {
+
+    let x = getRand(300)
+    let y = getRand(300)
+
+    settest((prev: any) => prev.concat({ label: '', x, y }))
+    console.log('button', { e });
+  }
+
+  const throttledmouse = throttle(handleMouseMove, 80)
+  const debouncedkey = debounce(handleKeyUp, 500)
+
+  useEffect(() => {
+    // This code runs whenever 'myProp' changes
+    // console.log('myProp has changed:');
+    // Perform any side effects here, e.g., fetching data, updating local state
+    // console.log({
+    //   menuX,
+    //   menuY
+    // });
+    console.log({ test });
+
+  }, [test]);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      onMouseDownCapture={handlemouseDown}
+      onMouseUp={handleMouseUp}
+      onMouseMove={throttledmouse}>
+      <Window label={'menu'} x={menuX} y={menuY} />
+      <input type="text" onKeyUp={debouncedkey} />
+      <button onClick={handleButton}>add window</button>
+
+      { test.map((t: any) => <Window x={t.x} y={t.y} />) }
     </div>
   );
 }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,51 +1,65 @@
-
-
+export { debounce as debounce } from './debounce'
+export { throttle as throttle } from './throttle'
 
 // class
 export class Test {
-    private message: string;
+  private message: string
 
-    constructor(mes: string){
-        this.message = mes;
-    }
+  constructor(mes: string) {
+    this.message = mes
+  }
 
-    greet(){
-        return 'sup, ' + this.message;
-    }
+  greet() {
+    return 'hi, ' + this.message;
+  }
 
-    reverse() {
-        // do reverse
-        this.message = this.message
-        .split('')
-        .reverse()
-        .join('')
+  reverse() {
+    // do reverse
+    this.message = this.message
+      .split('')
+      .reverse()
+      .join('')
 
-        return this;
-    }
+    return this;
+  }
 }
 
-//fix
-export function debounce(){
-    return function(){
+// trigger after delay of XXms
+// export function debounce(fn: Function, delay = 500) {
+  
+//   let timer: any
 
-    }
-}
+//   return function (this: any) {
 
-//fix
-export function throttle(){
-    let flag = false
-    return function(this: any){
-      let context = this
-      let args = arguments
+//     let context = this
+//     let args = arguments
 
-      if (flag){
-        flag = true
-        setTimeout(function(){
-          flag = false
-        }, 500)
-      }
-    }
-}
+//     clearTimeout(timer)
+//     timer = setTimeout(function() {
+//       fn.apply(context, args)
+//     }, delay)
+//   }
+// }
+
+// trigger to always after every XXms
+// export function throttle(fn: Function, limit = 500) {
+
+//   let flag = true
+
+//   return function (this: any) {
+//     const context = this
+//     const args = arguments
+
+//     if (flag) {
+//       fn.apply(context, args)
+//       flag = false
+      
+//       setTimeout(function () {
+//         flag = true
+//       }, limit)
+//     }
+//   }
+// }
 
 // export const throttle2 = () => {
 //     let flag = false;
