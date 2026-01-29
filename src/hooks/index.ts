8b048f33f9998d9d817d9d0431c7e97e972e181c
tytia77 <tytia77@gmail.com>
2026-01-29 16:29:19 +1100

    add lodash

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,16 +1,14 @@
-
-export * from './useArray'
-export * from './useAsync'
-export * from './useDebounce'
-export * from './useEventListener'
-export * from './useFetch'
-export * from './usePrevious'
-
-export * from './useStateWithHistory'
-export * from './useStorage'
-export * from './useTimeout'
-
-export * from './useToggle'
-
-export * from './useUpdateEffect'
-export * from './useWindowSize'
+export * from "./useArray";
+export * from "./useAsync";
+export * from "./useDebounce";
+export * from "./useEventListener";
+export * from "./useFetch";
+export * from "./usePrevious";
+export * from "./useStateWithHistory";
+export * from "./useStorage";
+export * from "./useThrottle";
+export * from "./useTimeout";
+export * from "./useToggle";
+export * from "./useUpdateEffect";
+export * from "./useWindowSize";
+export * from "./useDeepCompareEffect";
--- src/hooks/useDeepCompareEffect.ts
+++ src/hooks/useDeepCompareEffect.ts
@@ -1,12 +1,12 @@
-import { useEffect, useRef } from "react"
-// import isEqual from "lodash/fp/isEqual"
+import { useEffect, useRef } from "react";
+import isEqual from "lodash/fp/isEqual";
 
 export function useDeepCompareEffect<T>(callback: () => void, dependencies: T) {
-  const currentDependenciesRef = useRef()
+  const currentDependenciesRef: any = useRef();
 
-//   if (!isEqual(currentDependenciesRef.current, dependencies)) {
-//     currentDependenciesRef.current = dependencies
-//   }
+  if (!isEqual(currentDependenciesRef.current, dependencies)) {
+    currentDependenciesRef.current = dependencies;
+  }
 
-  useEffect(callback, [currentDependenciesRef.current])
+  useEffect(callback, [currentDependenciesRef.current]);
 }
